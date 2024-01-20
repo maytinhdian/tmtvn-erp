fbbb9f0f2c1047e267d64af4e24f4f55305a9eaf
Lê Thanh Nhã <tnhalk@maytinhdian.com>
2024-01-20 20:55:45 +0700

    API Sanctum login logout signup

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,8 +12,14 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
+ */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/signup', [AuthController::class, 'sign_up']);
+Route::post('/login', [AuthController::class, 'login']);
+// Route::post('/logout', [AuthController::class, 'logout']);
+
+// private routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    // logout
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
