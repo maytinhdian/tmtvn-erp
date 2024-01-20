6f459a21a7af0e41f316f625d17fdb7aab73eedf
Lê Thanh Nhã <tnhalk@maytinhdian.com>
2024-01-20 21:03:26 +0700

    API Sanctum user table seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+        DB::table('users')->insert([
+            'name'=>'Le Thanh Nha',
+            'email'=>'tnhalk@maytinhdian.com',
+            'password'=>Hash::make('123456'),
+            'id'=>0,
+            'created_at'=>Date('Y-m-d H:i:s'),
+            'updated_at'=>Date('Y-m-d H:i:s'),
+        ]);
     }
 }
---
+++ note.md
@@ -0,0 +1,20 @@
+# TMT Assets Management System
+
+## Cai dat goi laravel modules packages
+
+- composer require nwidart/laravel-modules
+- php artisan vendor:publish --provider="Nwidart\Modules\LaravelModulesServiceProvider"
+- composer dump-autoload
+- Link tài liệu tham khảo
+        <https://nwidart.com/laravel-modules/v6/basic-usage/creating-a-module>
+
+### Câu lệnh sử dụng cơ bản
+
+- php artisan module:make 'module-name'
+- php artisan module:make-migration create_products_table Product
+- php artisan module:seed --class=TenClassSeeder Product
+
+### Schema create table  
+
+- $table -> enum('is_saleable',['yes','no'])
+- $table -> foreignId('created_by')->constrained('users')
